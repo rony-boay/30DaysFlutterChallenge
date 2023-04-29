5ae9565d4542b000cc682ab7c96e4cce0d77167a
Rony Boay <ronyboay@gmail.com>
2023-04-29 08:38:15 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/crashcourse_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crashcourse_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text('first app'),
+      ),
+    );
+  }
+}
